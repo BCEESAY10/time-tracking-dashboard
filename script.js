6467de3891c8698aca3f12579309ae1e50a60507
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-16 02:01:17 +0000

    fetch data

--- script.js
+++ script.js
@@ -4,8 +4,18 @@
 
 const filterButtons = document.querySelectorAll('button');
 
+// Fectching Data
+fetch('/data.json').then((response) => {  
+  if(!response.ok) return console.log('Oops! Something went wrong.');
+  
+  return response.json();
+}).then((data) => {
+  console.log(data);
+});
+
 filterButtons.forEach(button => {
     button.addEventListener('click', () => {
-        alert("Good")
+        console.log(button.textContent.trim())
     })
 });
+
